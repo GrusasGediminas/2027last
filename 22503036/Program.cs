b0448e7f5d676ad128850f074b898379b17b73b3
TadeBerry <tc22503036@gmail.com>
2026-02-12 14:15:20 +0900

    Push2

--- 22503036/Program.cs
+++ 22503036/Program.cs
@@ -1,9 +1,20 @@
-﻿var number = 0;
-if (number is int)
+﻿using System;
+
+class Program
 {
-    Console.WriteLine("This is an integer.");
-}
-else
-{
-    Console.WriteLine("This is not an integer.");
+    static void Main()
+    {
+        Console.WriteLine("Write something");
+
+        string UserInput = Console.ReadLine();
+
+        if (int.TryParse(UserInput, out int number))
+        {
+            Console.WriteLine($"'{UserInput}' is an integer.");
+        }
+        else
+        {
+            Console.WriteLine($"'{UserInput}' is not an integer.");
+        }
+    }
 }
